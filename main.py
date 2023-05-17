de7111b321d1df6d7726d4737ac512c7cccbfab2
Sergey Novikov <sinkaiya@gmail.com>
2023-05-17 17:51:03 +0300

    The function can build the list of vacancies now.

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.common.exceptions import NoSuchElementException
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
@@ -24,7 +27,7 @@ def selenium_parser(job_title):
 
     # Setting browser settings. Making it so that it opens without a window, in the background.
     browser_options = Options()
-    browser_options.add_argument('--headless')
+    # browser_options.add_argument('--headless')
 
     # Creating a browser instance.
     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=browser_options)
@@ -44,62 +47,37 @@ def selenium_parser(job_title):
     # Submitting search request.
     search_input.submit()
 
-    # Ищем данные с полученной страницы (количество вакансий).
-    vacancies_count = browser.find_element(By.CSS_SELECTOR, '[data-qa="vacancies-search-header"]')
-    v_c = vacancies_count.text
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    # vacancies_count_object = browser.find_element(By.CSS_SELECTOR, '[data-qa="vacancies-search-header"]')
+    # vacancies_count_text = vacancies_count_object.text
+    #
+    # vacancies_count = str()
+    #
+    # for sym in vacancies_count_text:
+    #     if sym != ' ':
+    #         vacancies_count += sym
+    #     else:
+    #         break
+    #
+    # vacancies_count = int(vacancies_count)
+    #
+    # for vacancy in range(vacancies_count):
+    #     current_vacancy_object = browser.find_element(By.CSS_SELECTOR, '[data-qa="serp-item__title"]')
+    #     current_vacancy_text = current_vacancy_object.text
+    #     print(vacancy, current_vacancy_text)
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    vacancies = browser.find_elements(By.CSS_SELECTOR, '[data-qa="serp-item__title"]')
+    for vacancy in vacancies:
+        print()
+        print(vacancy.text)
+        print(vacancy.get_attribute('href'))
 
     time.sleep(3)
     browser.close()
-    return v_c
+    # return vacancies_count_text
 
 
-# Update - класс в рамках библиотеки telegram.Update, показывающий,
-# какие события получает наш handler (обработчик). Через update: Update
-# мы указываем функции, какой тип данных мы ожидаем в этом аргументе.
-# Второй обязательный аргумент, который мы в данном случае не используем,
-# мы просто заменяем на _, чтобы не вылезала ошибка.
-async def hello(update: Update, _):
-    # Ищем сообщение пользователя.
-    user_text = update.message.text
+current_job_title = 'python junior'
+selenium_parser(current_job_title)
 
-    # Из полученной от пользователя информации берём его имя и извлекаем оттуда first_name.
-    user_nickname = update.effective_user.name
-    user_first_name = update.effective_user.first_name
-    user_last_name = update.effective_user.last_name
-    print(f"Пользователь {user_first_name} {user_nickname} {user_last_name} отправил сообщение '{user_text}'.")
-
-    # Отвечаем пользователю: получаем событие, видим, что это было сообщение (message),
-    # и пытаемся вернуть текст (reply_text).
-    await update.message.reply_text(f"Пользователь {user_first_name} {user_nickname} {user_last_name} "
-                                    f"отправил сообщение '{user_text}'.")
-    await update.message.reply_text("Ищу информацию...")
-    try:
-        # Применяем функцию с парсером.
-        offers = selenium_parser(user_text)
-        print(f'По запросу {user_text} результат - {offers}.')
-
-        # Отдаём ответ.
-        await update.message.reply_text(f'По запросу "{user_text}" результат: {offers}.')
-    except NoSuchElementException as exc:
-        # Обрабатываем некорректный запрос.
-        print(f'ничего не найдено.')
-        await update.message.reply_text(f'По запросу {user_text} ничего не найдено.')
-
-
-def main():
-    # Создаём приложение.
-    app = ApplicationBuilder().token(TOKEN).build()
-
-    # Добавляем обработчик сообщений. Фильтр - сущность в данной библиотеке, подсказывающая,
-    # какого типа сообщение мы будем ждать от пользователя (текст, изображение, стикер, файл,
-    # аудио (голосовое сообщение) и т.д. Вторым аргументом указывается, с помощью какой
-    # функции мы будем обрабатывать сообщение.
-    app.add_handler(MessageHandler(filters.TEXT, hello))
-
-    # Запускаем бота.
-    app.run_polling()
-
-
-# Обеспечиваем корректный запуск файла из терминала.
-if __name__ == '__main__':
-    main()
