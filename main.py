f719e330782057cdaa0f13b04afe817dfad1a096
Sergey Novikov <sinkaiya@gmail.com>
2023-06-19 18:04:02 +0300

    Dev: 'add' button added, some tests performed, some code optimized.

--- main.py
+++ main.py
@@ -31,8 +31,6 @@ dp = Dispatcher(bot=bot, storage=MemoryStorage())
 # the StatesGroup class. The attributes within this class should be the instances
 # of the State() class.
 class GetUserData(StatesGroup):
-    # waiting_for_jobs = State()
-    # waiting_for_stops = State()
     waiting_for_jobs_or_stops = State()
     waiting_for_data_to_edit = State()
 
@@ -53,11 +51,11 @@ async def acquire_data(message, state, table):
     # If the user has sent not text but something weird, we are asking
     # to send text only. The state the bot currently in stays the same,
     # so it continues to wait for user's data.
-    if message.content_type != 'text' or message.content_type.startswith('/'):
+    if message.content_type != 'text' or message.text.startswith('/'):
         await message.answer(texts.only_text_warning)
         return
     # Saving the data in the FSM storage via the update_data() method.
-    await state.update_data(data_str=message.text)  # IS THIS NECESSARY???
+    # await state.update_data(data_str=message.text)  # IS THIS NECESSARY???????????????????????
     # Splitting a string into pieces, using a comma as separator and removing spaces.
     data_list = re.split(r',\s*', message.text)
     db_update_result = db.add_jobs_or_stops(table, data_list)
@@ -91,19 +89,20 @@ async def cmd_add_jobs_or_stops(message: types.Message, state: FSMContext):
 @dp.message_handler(state=GetUserData.waiting_for_jobs_or_stops, content_types='any')
 async def jobs_or_stops_acquired(message: types.Message, state: FSMContext):
     logging.info('The \'jobs_or_stops_acquired\' function has been started.')
-    command = state.get_data('command')  # RuntimeWarning: coroutine 'FSMContext.get_data' was never awaited
-    print(command)
+    command = await state.get_data()
+    command = command['command']
+    telegram_id = str(message.from_user.id)
     table = str()
     if command == '/add_jobs':
-        table = '_'.join(['jobs', str(message.from_user.id)])  # Or pass only template itself
+        table = '_'.join(['jobs', telegram_id])
     if command == '/add_stops':
-        table = '_'.join(['stops', str(message.from_user.id)])
+        table = '_'.join(['stops', telegram_id])
     data_acquired = await acquire_data(message, state, table)
     if data_acquired and command == '/add_jobs':
-        await message.answer(texts.jobs_list_acquired)
+        await message.answer(texts.jobs_acquired)
     if data_acquired and command == '/add_stops':
-        await message.answer(texts.stops_acquired)  # NAME!!!
-    else:
+        await message.answer(texts.stops_acquired)
+    if not data_acquired:
         await message.answer(texts.bot_error_message)
     await state.finish()
 
@@ -113,6 +112,7 @@ async def show_jobs_or_stops(message: types.Message):
     logging.info('The \'show_jobs_or_stops\' function has been started.')
     # Setting default inline buttons which are glued to each separate message
     buttons = [
+        types.InlineKeyboardButton(text="Добавить", callback_data='add_to_dataset'),
         types.InlineKeyboardButton(text="Изменить", callback_data='edit_dataset'),
         types.InlineKeyboardButton(text="Удалить", callback_data='delete_dataset')]
     table = str()
@@ -129,51 +129,75 @@ async def show_jobs_or_stops(message: types.Message):
     # and setting a proper table name we are acquiring the data from:
     if message.text == '/show_jobs':
         table = '_'.join(['jobs', telegram_id])
-        buttons[0]['callback_data'] = 'edit_jobs'
-        buttons[1]['callback_data'] = 'delete_jobs'
+        buttons[0]['callback_data'] = 'add_jobs'
+        buttons[1]['callback_data'] = 'edit_jobs'
+        buttons[2]['callback_data'] = 'delete_jobs'
     if message.text == '/show_stops':
         table = '_'.join(['stops', telegram_id])
-        buttons[0]['callback_data'] = 'edit_stops'
-        buttons[1]['callback_data'] = 'delete_stops'
+        buttons[0]['callback_data'] = 'add_stops'
+        buttons[1]['callback_data'] = 'edit_stops'
+        buttons[2]['callback_data'] = 'delete_stops'
 
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(*buttons)
     # Getting the data from the DB as a string and displaying it, keyboard buttons included.
     data_str = db.get_jobs_or_stops(table)
-    await message.answer(data_str, reply_markup=keyboard)
+    if data_str:
+        await message.answer(data_str, reply_markup=keyboard)
+    else:
+        await message.answer(texts.stops_empty)
 
 
 # A callback handler which catches the commands from the inline keyboard.
-@dp.callback_query_handler(lambda call: call.data in ['edit_jobs', 'delete_jobs',
-                                                      'edit_stops', 'delete_stops'], state='*')
+@dp.callback_query_handler(lambda call: call.data in ['add_jobs', 'edit_jobs', 'delete_jobs',
+                                                      'add_stops', 'edit_stops', 'delete_stops'],
+                           state='*')
 async def cmd_edit_or_delete_jobs_or_stops(callback_query: types.CallbackQuery, state: FSMContext):
     logging.info('The \'cmd_edit_or_delete_jobs_or_stops\' function has been started.')
     # The contents of the message which the button is glued .
     data_str_old = callback_query.message.text
     # The user's telegram id.
-    telegram_id = callback_query.message.chat.id
+    telegram_id = str(callback_query.message.chat.id)
     # The name of the command the button sent, like 'edit_job_names'.
     command = callback_query.data
 
     # Getting the id of the message we are about to edit:
     await bot.answer_callback_query(callback_query.id)
 
-    # Showing the user the dataset we are about to edit, and asking to enter a new one:
-    if command.startswith('edit'):
-        await bot.send_message(telegram_id, f'Изменяем следующие данные:\n\n{data_str_old}' +
-                               texts.edit_message)
+    logging.info('The callback query data acquired.')
+
+    if command.startswith('add'):
+        command = '/' + command
+        await state.update_data(command=command)
+        if command == '/add_jobs':
+            await bot.send_message(telegram_id, texts.add_jobs)
+        if command == '/add_stops':
+            await bot.send_message(telegram_id, texts.add_stops)
+        await state.update_data(command=command)
+        await state.set_state(GetUserData.waiting_for_jobs_or_stops.state)
     if command.startswith('delete'):
-        await bot.send_message(telegram_id, f'Удаляем следующие данные:\n\n{data_str_old}')
-
-    # Saving the data we are about to edit or delete, the telegram id
-    # and the command we received into the FSM storage via the update_data() method:
-    await state.update_data(data_str_old=data_str_old)
-    await state.update_data(telegram_id=str(telegram_id))
-    await state.update_data(command=command)
-
-    # Putting the bot into the 'waiting_for_data_to_edit' statement:
-    # await state.set_state(GetUserData.waiting_for_data_to_edit.state)
-    await state.set_state(GetUserData.waiting_for_data_to_edit.state)
+        await bot.send_message(telegram_id, f'{texts.deleting_data}{data_str_old}')
+        table = '_'.join([command.split('_')[1], telegram_id])
+        table_cleanup = db.clean_up_db_table(table)
+        if table_cleanup:
+            await bot.send_message(telegram_id, texts.table_cleanup_message)
+        else:
+            await bot.send_message(telegram_id, texts.bot_error_message)
+        logging.info('Delete command got from the user.')
+    if command.startswith('edit'):
+        await bot.send_message(telegram_id, f'{texts.changing_data}'
+                                            f'`{data_str_old}`'
+                                            f'{texts.edit_message}', parse_mode='MarkdownV2')
+        logging.info('Edit command got from the user.')
+        # Saving the data we are about to edit or delete, the telegram id
+        # and the command we received into the FSM storage via the update_data() method:
+        await state.update_data(data_str_old=data_str_old)
+        await state.update_data(telegram_id=telegram_id)
+        await state.update_data(command=command)
+        logging.info('All the necessary data saved in the FSM storage.')
+        # Putting the bot into the 'waiting_for_data_to_edit' statement:
+        await state.set_state(GetUserData.waiting_for_data_to_edit.state)
+        logging.info('The bot put into the \'waiting_for_data_to_edit\' statement.')
 
 
 # This function is being called only from the 'waiting_for_data_to_edit' statement.
@@ -182,26 +206,20 @@ async def edit_or_delete_jobs_or_stops(message: types.Message, state: FSMContext
     logging.info('The \'edit_or_delete_jobs_or_stops\' function has been started.')
     # Restoring the data details from the FSM storage:
     data_details = await state.get_data()
-    print(data_details)
     data_str_old = data_details['data_str_old']
     command = data_details['command']
-    print(command)
     telegram_id = data_details['telegram_id']
-    table = '_'.join([command[5:], telegram_id])
-    print(table)
+    table = '_'.join([command.split('_')[1], telegram_id])
 
     # Cleaning up the table:
     table_cleanup = db.clean_up_db_table(table)
-
-    if table_cleanup and command.startswith('edit'):
+    if table_cleanup:
         # Filling it with new data:
         data_acquired = await acquire_data(message, state, table)
         if data_acquired:
             await message.answer(f'{data_str_old}\n\nизменено на\n\n{message.text}.')
         else:
             await message.answer(texts.bot_error_message)
-    elif command.startswith('delete') and table_cleanup:
-        await message.answer(texts.table_cleanup_message)
     else:
         await message.answer(texts.bot_error_message)
     await state.finish()
